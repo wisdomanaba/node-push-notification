e98b0b71ea59ec357b72d696070f015c76bcca7e
wizzydev <wisdomanaba83@gmail.com>
2020-04-05 01:40:42 +0100

    Changed point

--- push.js
+++ push.js
@@ -5,8 +5,10 @@ let vapID = {
       privateKey: 'ExCDMW1H0VXjnB2QnscF-caj4A3uEr8Lunb2fBBb1iU'
 }
 
-push.setVapidDetails('wisdomanaba83@gmail.com', vapID.publicKey, vapID.privateKey)
+push.setVapidDetails('mailto:example@ff.com', vapID.publicKey, vapID.privateKey)
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dLe_EDNbaEc:APA91bGXxF0-pGoPav1bUB2tdPL9TvjdP6Xi4Am8laqOzjF2dpwVBonXByekKHOmjMhYhpwoAHvRR15HSC7sDbjLufJMzPFwpNUOW3Ld2F_DVkcAnKTYk6_xUd4i9bxXVWjqRzcU3bOm","expirationTime":null,"keys":{"p256dh":"BFiTG7dMS0zvzjBcqPTERkSkoR_y9jWYnG2W4z_rGCS5p7RnsKF2MAjghDRSZstg75lWPJRl9c0AIqOMIWLYyFc","auth":"em2hK9oPoQ4ZCHGfwbn1XQ"}}
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fdahsj1Y4Bs:APA91bHxjYpV2wPIxv5nmsmM4_qZaqgaEyyYJHFjnf7dR2wbBIrcZXyhhKbLg-RxxHU1JdyXdr-GGYMvQQ_XM-oaN4JY82MsCFMeyDF1vcfweD7c3VEXyX-aLnsONFyrjaZKcgyulu1U","expirationTime":null,"keys":{"p256dh":"BKTvxy_KHBBESA6gUaDRjHRmUy2QSLQ1AsaJprdgcF42UJyS2eppWQGewYYMwbrXaqyyIitUwVUAimJdk1acZ-4","auth":"CkRIf35CKFSu6PURIrHxRg"}}
+
+// let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fbmSn9k5Bck:APA91bGbi7raBzBKuYf308omwJToYoM4EidaD9YDMaWOSwwtJxAgSNxxmx6hsIDzD1kWexxOYhJGGCdX_6b24KLHuWDsqYwVqk-Krus8nPEvHjOMdc4IZLDKFkjLqoY-OCTptLTM7MVp","expirationTime":null,"keys":{"p256dh":"BH3BIc043IC7eNKzahng87djfWXnmoc-4xjU9dVWeRKmVdHsUVG5KaDMR_LjGpSnrX6h6W_rdNj1qTT-z2us0eM","auth":"2Bi88gX72SkaDBvF4VIxxA"}}
 
 push.sendNotification(sub, 'test message')
